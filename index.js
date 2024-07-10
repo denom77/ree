98ca0edeac3dd2ea3eef3a40fff1f4c81b49fea0
denom77 <157441587+denom77@users.noreply.github.com>
2024-07-10 12:06:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ client.on('ready', async () => {
     console.log(`${client.user.tag} - rich presence started!`);
 
     const r = new Discord.RichPresence()
-        .setApplicationId('1259800867385905243')
+        .setApplicationId('1260460472164159610')
         .setType('PLAYING')
-        .setURL('https://discord.gg/ZSvn6Zt5uT')
-        .setState('& Akun. Aman & Terpercaya')
-        .setName('Kei Store')
-        .setDetails('Menjual Jasa Top up, Jasa Joki')
-        .setAssetsLargeImage('https://media.discordapp.net/attachments/1190619079011225670/1259800090323980378/c49e2ffe-b8c6-4be0-82d6-5d5397df6f76.jpg?ex=668cffc3&is=668bae43&hm=ba116753676e9aa1a77b539b53ad59855d1677a14315535e60a7e3dff6743b2e&=&format=webp&width=422&height=422')
+        .setURL('https://discord.com/invite/SuMxrQ8q8q')
+        .setState('& Menjual Fruit')
+        .setName('Ree Store')
+        .setDetails('Jasa Joki Blox Fruit')
+        .setAssetsLargeImage('https://cdn.discordapp.com/attachments/990796154201587782/1260461127238680606/fawf.png?ex=668f6766&is=668e15e6&hm=b72bf6a271897dee9f304995a57a04387892feac35b19cfb47fb99b4f6e7a164&')
         .setAssetsLargeText('Trusted')
-        .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1190619079011225670/1259798167516483584/vraje.gif?ex=668cfdf9&is=668bac79&hm=48a6416e5736267e0a698da7a24fad938e0f38c5aed8d7cb39e480ae1b25e1ad&')
+        .setAssetsSmallImage('https://cdn.discordapp.com/attachments/990796154201587782/1260461158826246174/1196765932119408711.gif?ex=668f676e&is=668e15ee&hm=5b1aa8de4d86741797bcdc513ac87f9c9d8de91cbd3fcb150141ea8db55f5a3d&')
         .setAssetsSmallText('Verified')
-        .addButton('Join Discord Server', 'https://discord.gg/ZSvn6Zt5uT')
-        .addButton('Order? Wa', 'https://wa.me/6282132910644');
+        .addButton('Ree Store Server [JOKI ROBLOX]', 'https://discord.com/invite/SuMxrQ8q8q')
+        .addButton('Tanya - tanya', 'https://discord.com/invite/SuMxrQ8q8q');
 
     client.user.setActivity(r);
     client.user.setPresence({ status: 'idle' });
